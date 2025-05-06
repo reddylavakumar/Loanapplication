9cecf2a104d5298b4a4a822fc5dcff5ca28bbc02
lavakumar <lavapkumar@gmail.com>
2025-05-06 20:59:41 +0530

    Theme change

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 const App = () => {
-  const [mode, setMode] = useState("dark");
+  const [mode, setMode] = useState("light");
 
   const theme = useMemo(
     () =>
